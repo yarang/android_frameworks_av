962643927fd364e6003a4c219a9661622bcd86e1
secuflag <secuflag@ticino.com>
2016-06-24 17:53:49 +0200

    Enable SW video decoder
    * See here: https://github.com/peyo-hd/device_brcm_rpi2/wiki#enable-sw-video-decoder
    
    Change-Id: I385008d64157f2780735827b10217d970a7e4fc7
    Signed-off-by: secuflag <secuflag@ticino.com>

--- media/libstagefright/colorconversion/SoftwareRenderer.cpp
+++ media/libstagefright/colorconversion/SoftwareRenderer.cpp
@@ -112,9 +112,6 @@ void SoftwareRenderer::resetFormatIfChanged(const sp<AMessage> &format) {
             case OMX_COLOR_FormatYUV420SemiPlanar:
             case OMX_TI_COLOR_FormatYUV420PackedSemiPlanar:
             {
-                halFormat = HAL_PIXEL_FORMAT_YV12;
-                bufWidth = (mCropWidth + 1) & ~1;
-                bufHeight = (mCropHeight + 1) & ~1;
                 break;
             }
             case OMX_COLOR_Format24bitRGB888:
